867eeb01a2e682d5013074a127ed186cee6ba175
diane <student@epicodus.com>
2016-02-17 17:05:15 -0800

    refactoring wip.

--- js/-interface.js
+++ js/-interface.js
@@ -1,5 +1,18 @@
 var module = require('./../js/module.js');
+initMap = module.initMap;
 $(document).ready(function(){
   console.log('yo! I am working!');
   module.hello();
+  $('#go').click(function(){
+    // $.get("https://maps.googleapis.com/maps/api/js?key=AIzaSyAfuASVNuk6GPEDuEiqaXpeI4uT4rFKTnA&callback=initMap", function(response){
+    //   console.log(response)
+    // });
+    $.get(
+      url: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAfuASVNuk6GPEDuEiqaXpeI4uT4rFKTnA",
+      data: data,
+      success: initMap,
+      dataType: 'json',
+      origin: "http:"
+    );
+  });
 });
--- js/module.js
+++ js/module.js
@@ -1,3 +1,21 @@
 exports.hello = function(){
   console.log('hello');
 }
+
+exports.initMap = function() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  // Create a map object and specify the DOM element for display.
+  exports.map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 4
+  });
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: exports.map,
+    position: myLatLng,
+    title: 'Hello World!'
+  });
+}
